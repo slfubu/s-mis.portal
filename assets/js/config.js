09e1f7a1b9b26243e16bad6974b141ef7f160331
slfubu <thapanutputtathakorn@gmail.com>
2026-09-07 02:03:16 +0700

    Add files via upload

---
+++ assets/js/config.js
@@ -0,0 +1,8 @@
+// S-MIS PORTAL - GitHub Pages configuration
+// ใส่ URL Deployment ของ Google Apps Script Web App ที่ลงท้ายด้วย /exec
+// ตัวอย่าง: https://script.google.com/macros/s/AKfycbXXXXXXXXXXXXXXXXXXXXXXXX/exec
+window.APP_CONFIG = Object.freeze({
+  API_URL: 'PASTE_YOUR_DEPLOYMENT_EXEC_URL_HERE',
+  REQUEST_TIMEOUT_MS: 30000,
+  DEBUG_API: false
+});
