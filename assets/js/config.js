3976014ef40e5f23d52a0def06d2bc5556cba6aa
slfubu <thapanutputtathakorn@gmail.com>
2026-09-07 02:04:25 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,7 +2,7 @@
 // ใส่ URL Deployment ของ Google Apps Script Web App ที่ลงท้ายด้วย /exec
 // ตัวอย่าง: https://script.google.com/macros/s/AKfycbXXXXXXXXXXXXXXXXXXXXXXXX/exec
 window.APP_CONFIG = Object.freeze({
-  API_URL: 'PASTE_YOUR_DEPLOYMENT_EXEC_URL_HERE',
+  API_URL: 'https://script.google.com/macros/s/AKfycbxo2E2dWkw1UaFguCH3FLyk4S3vZL7-ev4BGigkNaXlXreJCqXHktKdFVmoIFJmUdt6/exec',
   REQUEST_TIMEOUT_MS: 30000,
   DEBUG_API: false
 });
